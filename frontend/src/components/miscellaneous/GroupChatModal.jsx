b9346610dffa22f357fa6050a9106648d45a0795
Amaresh Barik <96060638+amareshcoding@users.noreply.github.com>
2022-12-16 12:28:11 +0530

    single chat complted and group chat is in progress

--- frontend/src/components/miscellaneous/GroupChatModal.jsx
+++ frontend/src/components/miscellaneous/GroupChatModal.jsx
@@ -1,5 +1,7 @@
 import {
   Button,
+  FormControl,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -7,12 +9,61 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Spinner,
   useDisclosure,
+  useToast,
 } from '@chakra-ui/react';
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { ChatState } from '../../context/ChatProvider';
+import UserListItem from '../userItems/UserListItem';
 
 const GroupChatModal = ({ children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [groupChatName, setGroupChatName] = useState('');
+  const [selectedUser, setSelectedUser] = useState([]);
+  const [search, setSearch] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+
+  const { user, chats, setChats } = ChatState();
+
+  const handleSearch = async (query) => {
+    setSearch(query);
+
+    if (!query) return;
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+
+      setLoading(false);
+      setSearchResult(data);
+    } catch (err) {
+      toast({
+        title: 'Error Occured!',
+        description: 'Faild to Load the Search Result',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+        position: 'bottom-left',
+      });
+    }
+  };
+
+  const hanleGroup = (user) => {
+    //
+  };
+
+  const handleSubmit = () => {
+    //
+  };
+
   return (
     <>
       <span onClick={onOpen}>{children}</span>
@@ -20,22 +71,50 @@ const GroupChatModal = ({ children }) => {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader
+            fontSize="35px"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent="center"
+          >
+            Create Group Chat
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-              error sapiente deleniti libero voluptate laborum doloribus
-              perspiciatis a, quisquam, illum dicta rerum praesentium obcaecati
-              modi sunt eius consequatur reprehenderit nisi.
-            </p>
+          <ModalBody display="flex" flexDir="column" alignItems="center">
+            <FormControl>
+              <Input
+                placeholder="Chat Name"
+                mb={3}
+                onChange={(e) => setGroupChatName(e.target.value)}
+              />
+            </FormControl>
+            <FormControl>
+              <Input
+                placeholder="Add Users eg: Amaresh, Guest ..."
+                mb={1}
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </FormControl>
+            {/* selected users */}
+            {loading ? (
+              <Spinner ml={'auto'} display="flex" />
+            ) : (
+              searchResult
+                .slice(0, 4)
+                .map((user) => (
+                  <UserListItem
+                    key={user._id}
+                    user={user}
+                    handleFunction={() => hanleGroup(user)}
+                  />
+                ))
+            )}
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
+            <Button colorScheme="blue" onClick={handleSubmit}>
+              Create Chat
             </Button>
-            <Button variant="ghost">Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -9,7 +9,7 @@ const ChatPage = () => {
   return (
     <div style={{ width: '100%' }}>
       {user && <SideDrawer />}
-      <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+      <Box display="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
         {user && <MyChats />}
         {user && <ChatBox />}
       </Box>
